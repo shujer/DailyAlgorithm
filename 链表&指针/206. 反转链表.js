5c9ba5e6adc4d6bab010343dd3eaa9df63e39dda
shujer <dwellerofuniverse@gmail.com>
2021-04-13 22:15:30 +0800

    feat: add 206

---
+++ 链表&指针/206. 反转链表.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let prev = null,
+    cur = head;
+  while (cur) {
+    let tmp = cur.next;
+    cur.next = prev;
+    prev = cur;
+    cur = tmp;
+  }
+  return prev;
+};
